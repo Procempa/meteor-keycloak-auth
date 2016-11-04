85120fb722473e0fc290c826772fa47dd48c47ce
Alex Gouvea Vasconcelos <alexgv@procempa.com.br>
2016-11-04 11:51:56 -0200

    Transformar token numa promise

--- keycloak-server.js
+++ keycloak-server.js
@@ -4,34 +4,40 @@ import _ from 'lodash';
 import { Config, GrantManager } from 'keycloak-auth-utils';
 import { EJSON } from 'meteor/ejson';
 import Future from 'fibers/future';
+import Q from 'q';
 /* global Assets*/
 
 
 export class KeycloakServerImpl {
 	user;
 	grant;
-	bearer_token;
+	tokenDeferred = Q.defer();
 
-	constructor(server) {
+	get bearer_token() {
+		return this.tokenDeferred.promise;
+	}
+
+	constructor( server ) {
 		this._server = server || Meteor.server;
-		let keycloakJson = EJSON.parse(Assets.getText('../../private/keycloak.json'));
-		let keycloakConfig = new Config(keycloakJson);
-		let grantManager = new GrantManager(keycloakConfig);
-		this._server.onConnection((connection) => {
-			let session = this._server.sessions[connection.id];
+		let keycloakJson = EJSON.parse( Assets.getText( '../../private/keycloak.json' ) );
+		let keycloakConfig = new Config( keycloakJson );
+		let grantManager = new GrantManager( keycloakConfig );
+		this._server.onConnection( ( connection ) => {
+			let session = this._server.sessions[ connection.id ];
 			let socket = session.socket;
 			let processMessage = socket._meteorSession.processMessage;
-			socket._meteorSession.processMessage = Meteor.bindEnvironment((msg_in) => {
-				if (_.indexOf(DISCARDED_MESSAGES, msg_in.msg) === -1 && msg_in.raw_token) {
-					let grant = grantManager.createGrant(msg_in.raw_token);
+			socket._meteorSession.processMessage = Meteor.bindEnvironment( ( msg_in ) => {
+				if ( _.indexOf( DISCARDED_MESSAGES, msg_in.msg ) === -1 && msg_in.raw_token ) {
+					let grant = grantManager.createGrant( msg_in.raw_token );
 					let fut = new Future();
-					grantManager.ensureFreshness(grant, (error) => {
-						if (error) {
-							fut.throws(error);
+					grantManager.ensureFreshness( grant, ( error ) => {
+						if ( error ) {
+							fut.throws( error );
+							this.tokenDeferred.reject( error );
 						} else {
 							fut.return();
 						}
-					});
+					} );
 					fut.wait();
 					this.grant = grant;
 					this.user = {
@@ -39,18 +45,21 @@ export class KeycloakServerImpl {
 						name: grant.id_token.content.name || grant.id_token.content.preferred_username,
 						email: grant.id_token.content.email,
 						roles: {
-							realm: ((grant.access_token.content.realm_access || {}).roles || []),
-							client: ((grant.access_token.content.resource_access[keycloakConfig.clientId] || {}).roles || [])
+							realm: ( ( grant.access_token.content.realm_access || {} )
+								.roles || [] ),
+							client: ( ( grant.access_token.content.resource_access[ keycloakConfig.clientId ] || {} )
+								.roles || [] )
 						}
 					};
-					this.bearer_token = grant.access_token.token;
+					this.tokenDeferred.resolve( grant.access_token.token );
+					//this.bearer_token = grant.access_token.token;
 				}
-				processMessage.apply(socket._meteorSession, [msg_in]);
-			});
-		});
+				processMessage.apply( socket._meteorSession, [ msg_in ] );
+			} );
+		} );
 	}
 
-	isInRole(roles, scope) {
-		return isInRole(this.user, roles, scope);
+	isInRole( roles, scope ) {
+		return isInRole( this.user, roles, scope );
 	}
 }
