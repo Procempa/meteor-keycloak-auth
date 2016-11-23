3a39a07cd918da458a437eb4e91cf5e590b26959
Alex Gouvea Vasconcelos <alexgv@procempa.com.br>
2016-11-23 17:46:11 -0200

    Removing obsolete logs

--- keycloak-server.js
+++ keycloak-server.js
@@ -1,4 +1,3 @@
-/*globals Assets*/
 import { Meteor } from 'meteor/meteor';
 import { ServiceConfiguration } from 'meteor/service-configuration';
 import { DISCARDED_MESSAGES, isInRole } from './common';
@@ -6,7 +5,6 @@ import _ from 'lodash';
 import { Config, GrantManager } from 'keycloak-auth-utils';
 import Future from 'fibers/future';
 import Q from 'q';
-import { EJSON } from 'meteor/ejson';
 
 export class KeycloakServerImpl {
 	user;
@@ -15,62 +13,61 @@ export class KeycloakServerImpl {
 	tokenDeferred = Q.defer();
 
 	get bearer_token() {
-		return _.get(this, 'tokenDeferred.promise');
+		return _.get( this, 'tokenDeferred.promise' );
 	}
 
-	constructor(server) {
+	constructor( server ) {
 		this._server = server || Meteor.server;
 
-		let keycloakJson = ServiceConfiguration.configurations.findOne({ 'service': 'keycloak' });
+		let keycloakJson = ServiceConfiguration.configurations.findOne( { 'service': 'keycloak' } );
 
-		if (keycloakJson) {
-			this.start(keycloakJson);
+		if ( keycloakJson ) {
+			this.start( keycloakJson );
 		} else {
-			console.log(`
+			console.log( `
 --------------------------------------------------------------------------------------------------------------------
 There is no 'service': 'keycloak' object in collection meteor_accounts_loginServiceConfiguration.
 You need to add an object with the same attributes as keycloak.json, plus the attribute
 'service': 'keycloak'
 in that collection so it starts working.
 --------------------------------------------------------------------------------------------------------------------
-			`);
+			` );
 			this.waitingConfig = true;
-			let observable = ServiceConfiguration.configurations.find({ 'service': 'keycloak' }).observe({
-				added: (config) => {
+			let observable = ServiceConfiguration.configurations.find( { 'service': 'keycloak' } ).observe( {
+				added: ( config ) => {
 					observable.stop();
-					this.start(config);
+					this.start( config );
 				}
-			});
+			} );
 		}
 	}
 
-	start(keycloakJson) {
-		console.log('STARTING');
-		_.unset(this, 'waitingConfig');
-		let keycloakConfig = new Config(keycloakJson);
-		console.info('Keycloak Server (Module):', keycloakConfig.authServerUrl);
+	start( keycloakJson ) {
+		_.unset( this, 'waitingConfig' );
+		let keycloakConfig = new Config( keycloakJson );
+		console.info( 'Keycloak Server (Module):', keycloakConfig.authServerUrl );
 
-		let grantManager = new GrantManager(keycloakConfig);
-		this._server.onConnection((connection) => {
-			let session = this._server.sessions[connection.id];
+		let grantManager = new GrantManager( keycloakConfig );
+		this._server.onConnection( ( connection ) => {
+			let session = this._server.sessions[ connection.id ];
 			let socket = session.socket;
 			let processMessage = socket._meteorSession.processMessage;
-			socket._meteorSession.processMessage = Meteor.bindEnvironment((msg_in) => {
-				if (_.indexOf(DISCARDED_MESSAGES, msg_in.msg) === -1 && msg_in.raw_token) {
+			socket._meteorSession.processMessage = Meteor.bindEnvironment( ( msg_in ) => {
+				if ( _.indexOf( DISCARDED_MESSAGES, msg_in.msg ) === -1 && msg_in.raw_token ) {
 					let realized = this.tokenDeferred.promise.inspect().state === 'fulfilled';
 					this.tokenDeferred = realized ? Q.defer() : this.tokenDeferred;
 					// console.log( '====> Token ' + ( realized ? 'novo' : '' ) + ': ', this.tokenDeferred );
 
-					let grant = grantManager.createGrant(msg_in.raw_token);
+					let grant = grantManager.createGrant( msg_in.raw_token );
 					let fut = new Future();
-					grantManager.ensureFreshness(grant, (error) => {
-						if (error) {
-							fut.throws(error);
-							this.tokenDeferred.reject(error);
+					grantManager.ensureFreshness( grant, ( error ) => {
+						if ( error ) {
+							fut.throws( error );
+							this.tokenDeferred.reject( error );
 						} else {
 							fut.return();
 						}
-					});
+					} );
 					fut.wait();
 					this.grant = grant;
 					this.user = {
@@ -78,21 +75,21 @@ in that collection so it starts working.
 						name: grant.id_token.content.name || grant.id_token.content.preferred_username,
 						email: grant.id_token.content.email,
 						roles: {
-							realm: ((grant.access_token.content.realm_access || {})
-								.roles || []),
-							client: ((grant.access_token.content.resource_access[keycloakConfig.clientId] || {})
-								.roles || [])
+							realm: ( ( grant.access_token.content.realm_access || {} )
+								.roles || [] ),
+							client: ( ( grant.access_token.content.resource_access[ keycloakConfig.clientId ] || {} )
+								.roles || [] )
 						}
 					};
-					this.tokenDeferred.resolve(grant.access_token.token);
+					this.tokenDeferred.resolve( grant.access_token.token );
 					//this.bearer_token = grant.access_token.token;
 				}
-				processMessage.apply(socket._meteorSession, [msg_in]);
-			});
-		});
+				processMessage.apply( socket._meteorSession, [ msg_in ] );
+			} );
+		} );
 	}
 
-	isInRole(roles, scope) {
-		return isInRole(this.user, roles, scope);
+	isInRole( roles, scope ) {
+		return isInRole( this.user, roles, scope );
 	}
 }
